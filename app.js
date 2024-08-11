c5148227bae7bc9cf0ca67d27e603893f1eab9c8
Trevor Bowman <bowma@Trevors-MacBook-Pro.local>
2024-08-11 10:39:24 -0400

    Initial backend commit

---
+++ app.js
@@ -0,0 +1,25 @@
+function searchTable() {
+    var guestName = document.getElementById('guestName').value;
+
+    if (!guestName) {
+        document.getElementById('result').innerHTML = "Please enter a guest name.";
+        return;
+    }
+
+    var url = "https://script.google.com/macros/s/AKfycbxKfT41pECdbnFxSljIJcCJcx5FPOn7oydo6UjqlRY/dev?guestName=" + encodeURIComponent(guestName);
+
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            if (data === "Not Found") {
+                document.getElementById('result').innerHTML = "Name not found. Please try again.";
+            } else {
+                document.getElementById('result').innerHTML = "Table Number: " + data;
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('result').innerHTML = "There was an error retrieving the table number.";
+        });
+}
+
